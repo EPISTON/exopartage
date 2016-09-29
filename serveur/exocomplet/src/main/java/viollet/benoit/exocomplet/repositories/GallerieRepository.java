a39d22963dfea59f2e238b952694b14e2446d970
violletbenoit <viollet.benoit@free.fr>
2016-09-29 14:43:29 +0200

    Reset du projet

--- serveur/exocomplet/src/main/java/viollet/benoit/exocomplet/repositories/GallerieRepository.java
+++
@@ -1,9 +0,0 @@
-package viollet.benoit.exocomplet.repositories;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import viollet.benoit.exocomplet.metier.Gallerie;
-
-public interface GallerieRepository extends PagingAndSortingRepository<Gallerie, Integer>{
-
-}
--- serveur/exocomplet/src/main/java/viollet/benoit/exocomplet/repositories/TagRepository.java
+++
@@ -1,9 +0,0 @@
-package viollet.benoit.exocomplet.repositories;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import viollet.benoit.exocomplet.metier.Tag;
-
-public interface TagRepository extends PagingAndSortingRepository<Tag, Integer>{
-	
-}
